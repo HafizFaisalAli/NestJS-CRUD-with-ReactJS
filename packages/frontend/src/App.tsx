44d2569e07345aaa70f2d70a42b47e7e68e4c490
hafizfaisalali <hafizfaisalali605@gmail.com>
2024-10-09 18:03:13 +0500

    feat: tanstack query basic setup with devtool

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
+
+const fetchData = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  return res.json();
+};
+
 const App = () => {
-  return <div>App</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['todo'],
+    queryFn: fetchData,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <button onClick={() => setIsOpen(!isOpen)}>
+        {`${isOpen ? 'Close' : 'Open'} the devtools panel`}
+      </button>
+      {isOpen && <ReactQueryDevtoolsPanel onClose={() => setIsOpen(false)} />}
+      <h1>Todo Item</h1>
+      <div>
+        {data?.map((item: any) => (
+          <ul key={item.id}>
+            <li>{item?.title}</li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
